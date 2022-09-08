930527a4ab3993ecccd7b3e5ff675c1e01146351
Amarajah <naomi.ibeh69@gmail.com>
2022-09-08 16:08:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,28 @@
-#include <stdio.h>
-
-#include <stdlib.h>
+#include <unistd.h>
 
 
 
 /**
  *
- *  * main - Entry point
+ *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *
- *   * Return: Always 1 (Success)
+ *   *                followed by a new line, to standard error.
  *
- *    */
+ *    *
+ *
+ *     * Return: Always 1.
+ *
+ *      */
 
 int main(void)
 
 {
+		write(2,
 
-		write(STDOUT_FILENO, "and that piece of art is useful
+		     "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 
-							\" - Dora Korpar, 2015-10-19\n", 59);
+					      	      59);
 
-			return (1);
+		return (1);
 
 }
