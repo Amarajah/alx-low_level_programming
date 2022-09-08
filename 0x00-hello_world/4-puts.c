78557200d9b1439ad99167254290dbd6d92b1489
Amarajah <naomi.ibeh69@gmail.com>
2022-09-08 16:57:45 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
+ *
+ *Description: 'the program's description'
  *
  * Return : Always 0 (Success)
  */
 int main(void)
 {
 	/*returns string*/
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 
-}	
+}
