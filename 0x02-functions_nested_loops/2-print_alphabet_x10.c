8c447f844e72648a536719f9cf77f29f155176c5
Amarajah <naomi.ibeh69@gmail.com>
2022-09-13 10:08:59 -0700

    *10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,17 @@
 
 void print_alphabet_x10(void)
 {
+	int count = 0;
+
 	char letter;
 
+	while (count++ <= 9)
+{
 	for (letter = 'a'; letter <= 'z'; letter++)
 
 	putchar(letter * 10);
 
 	putchar('\n');
+}
+
 }
