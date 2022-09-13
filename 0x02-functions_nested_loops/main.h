ae704d2b6d3e9e8d830bf67ba11aa42bdd9dc8b3
Amarajah <naomi.ibeh69@gmail.com>
2022-09-13 09:03:32 -0700

    main header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+/**
+ *file - main.h
+ *
+ *Author : Ibe Naomi
+ *
+ *Description: File containing all functions used in the nested loops directory
+ */
+
+int _putchar(char c);
+
+#endif
