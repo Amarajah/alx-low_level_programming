19331920e66cb9143b2e022e1301770c78cf5eb3
Amarajah <naomi.ibeh69@gmail.com>
2022-09-13 08:49:30 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - entry point of program to output character _putchar
+ *
+ *Description - a function to output '_putchar'
+ *
+ *Return: 0 (successful)
+ */
+
+int main(void)
+{
+	char ch = '_putchar';
+
+	putchar(ch\n);
+
+	return (0);
+}
