3a996736ba66aba4c3b18cd2c79e0a8bae83db0b
Amarajah <naomi.ibeh69@gmail.com>
2022-09-14 05:48:46 -0700

    n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  *print_times_table - prints times table
  *@n: integer
  *
  *
- *Return: 0
+ *Return: 0 (success)
  */
 
 void print_times_table(int n)
 {
+
+
 	return (0);
 }
