b2ffc29422caf26609ab150044308e5d567c2cf6
Amarajah <naomi.ibeh69@gmail.com>
2022-09-14 03:08:16 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *add - function to add two numbers together
+ *
+ *@n1: initial number
+ *@n2: second numeric value
+ *
+ *Return: Value of sum of n1 and n2
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
