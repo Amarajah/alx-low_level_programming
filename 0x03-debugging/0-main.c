8bcc64df4116d044b741e32f38f51b8a820a0783
Amarajah <naomi.ibeh69@gmail.com>
2022-09-15 01:40:17 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - writes code to check and print if a number is negative or positive
+ *
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative = (i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This project was done to do a practice of debugging codes and making sure they run as expected
