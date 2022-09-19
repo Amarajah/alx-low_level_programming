14840ea1a91e33df25ba5a0f024aac733498f9e0
Amarajah <naomi.ibeh69@gmail.com>
2022-09-19 08:12:50 -0700

    print-rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - function to print a string in reverse
+ *
+ * @s: string character
+ *
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int len = 0, index;
+
+	while (s[index++])
+
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
