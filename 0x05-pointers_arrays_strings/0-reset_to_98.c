016a5337d34f48abc45520fc5ee12835a869cc8d
Amarajah <naomi.ibeh69@gmail.com>
2022-09-19 06:54:05 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to accept integer parameter and point it to 98
+ *
+ * @n: integer
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
