c4df26f1778dc0bbc1593f964cce292ac26e6dde
Amarajah <naomi.ibeh69@gmail.com>
2022-09-19 08:20:34 -0700

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,15 +10,15 @@
 
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index = 0;
 
-	while (s[index++])
+	while (s[index])
 
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
-
+		index++;
+	while (index--)
+	{
 		_putchar(s[index]);
+	}
 
 	_putchar('\n');
 }
