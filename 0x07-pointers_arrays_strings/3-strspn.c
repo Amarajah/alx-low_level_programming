f752f457564e42c36d726a9a038dc312fb729840
Amarajah <naomi.ibeh69@gmail.com>
2022-09-26 14:49:48 -0700

    str

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,19 +15,19 @@ unsigned int _strspn(char *s, char *accept)
 
 	while (*s)
 	{
-	for (i = 0; accept[i]; i++)
-	{
-		if (*s == accept[i])
+		for (i = 0; accept[i]; i++)
 		{
-			bytes++;
-			break;
-		}
+			if (*s == accept[i])
+			{
+				bytes++;
+				break;
+			}
 
-		else if (accept[i + 1] == '\0')
-		{
-			return (bytes);
+			else if (accept[i + 1] == '\0')
+				return (bytes);
 		}
 		s++;
+
 	}
 	return (bytes);
 }
