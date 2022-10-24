6f7c58eec3ec2804dcaf981202b632a8aaa4572d
Amarajah <naomi.ibeh69@gmail.com>
2022-10-24 15:47:16 -0700

    singly-linked

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,18 +3,18 @@
 /**
  * add_nodeint - function that adds a new node at the beginning of a list
  * @n: linked list
- * @h: head of linked list
+ * @head: head of linked list
  * Return: the address of the new element, or NULL if it failed
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t * new_node = (listint_t *) malloc(sizeof(listint_t));
+	listint_t *new_node = (listint_t *) malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 	{
 		free(new_node);
-		return(NULL);
+		return (NULL);
 	}
 	new_node->n = n;
 
@@ -22,5 +22,5 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	(*head) = new_node;
 
-	return(new_node);
+	return (new_node);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,8 +9,8 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t* new_node =
-		(listint_t*)malloc(sizeof(listint_t));
+	listint_t *new_node =
+		(listint_t *) malloc(sizeof(listint_t));
 
 	listint_t *last = *head;
 
@@ -19,12 +19,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 		*head == new_node;
-		return(NULL);
+		return (NULL);
 
 	while (last->next != NULL)
 		last = last->next;
 
 	last->next = new_node;
 
-	return(new_node);
+	return (new_node);
 }
