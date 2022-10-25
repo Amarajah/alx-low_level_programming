cabd523cc08fd588b9fa80e5cabb8cab33b5dd32
Amarajah <naomi.ibeh69@gmail.com>
2022-10-25 01:09:25 -0700

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a list
+ * @head: list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new_node, *current_node;
+	current_node = *head;
+
+	if (head == NULL)
+		return;
+
+	while (current_node)
+	{
+		new_node = current_node->next;
+		free(current_node);
+		current_node = new_node;
+	}
+	*head = NULL;
+}
