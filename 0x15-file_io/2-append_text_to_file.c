72de2804cc98fb557194b95d67cb05bd775b59ca
Amarajah <naomi.ibeh69@gmail.com>
2022-12-18 12:05:48 -0800

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,15 +21,15 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_APPEND | O_WRONLY);
 
-		if (fd == -1)
-			return (-1);
+	if (fd == -1)
+		return (-1);
 
-		for (i = 0; text_content[i] != '\0'; i++);
+	for (i = 0; text_content[i] != '\0'; i++)
 		status = write(fd, text_content, i);
 
-			if (status == -1)
-				return (-1);
+	if (status == -1)
+		return (-1);
 
-		close(fd);
-		return (1);
+	close(fd);
+	return (1);
 }
