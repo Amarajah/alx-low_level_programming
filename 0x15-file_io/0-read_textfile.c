e14d850e035c81f076c9380085583cc415a4c88d
Amarajah <naomi.ibeh69@gmail.com>
2022-12-18 11:50:19 -0800

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,19 +15,27 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file = fopen(filename, "r");
+	ssize_t o, r, w;
+	char *buffer;
 
-	if (file == NULL) {
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer ==  NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
 		return (0);
 	}
+	free(buffer);
+	close(o);
 
-	for (size_t i = 0; i < letters; i++) {
-		char c = fgetc(file);
-		if (c == EOF) {
-			break;
-		}
-		 write(1, &c, 1);
-	}
-	fclose(file);
-	return letters;
+	return (w);
 }
