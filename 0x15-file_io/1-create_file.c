cb9f2bb27f74a3e10a3c839ba5fe60de4eeee4b1
Amarajah <naomi.ibeh69@gmail.com>
2022-12-18 12:49:01 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,10 +27,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (i = 0; text_content[i] != '\0'; i++);
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
 		rstatus = write(fd, text_content, i);
-
-			if (rstatus == -1)
-				return (-1);
+		if (rstatus == -1)
+			return (-1);
 	}
+	close(fd);
+	return (1);
 }
