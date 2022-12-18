3ffab4c314fca3b112b2161f47526e6de237d41a
Amarajah <naomi.ibeh69@gmail.com>
2022-12-18 15:08:44 -0800

    heads&tails

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,26 +8,32 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	dlistint_int *new_node, *temp;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
 
-	while (head->next != NULL)
-		head = head->next;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-	head->next = new_node;
-	new_node->prev = head;
+	temp->next = new_node;
+	new_node->prev = temp;
 
 	return (new_node);
 }
