ebf14681b771d81dbc771278fce75dfe3c09fed9
Amarajah <naomi.ibeh69@gmail.com>
2023-01-07 02:20:57 -0800

    hash

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: hash table you want to add or update the key/value
+ * @key: hash key which cannot be empty
+ * @value: value associated with the key
+ * value must be duplicated. value can be an empty string
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int hash = hash_djb2(key);
+
+	return hash % size;
+}
